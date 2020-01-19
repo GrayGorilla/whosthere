f9a64a8c51daa8608d89e72e682826b5064974f6
GrayGorilla <nbren004@ucr.edu>
2020-01-19 12:24:49 +0000

    added the camera and google cloud functionality in main

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ while True:
     motion = pir.motion_detected
     if (motion):
         print("Motion Detected")
-        fileName, person, package = personOrPackage
+        fileName, person, package = personOrPackage()
         print("Person present?: ", person)
         print("Package present?: ", package)
     else:
