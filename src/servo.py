acde9e53be3b00c17cf1d6ac4da81ea47f178f01
SeungEunHa <sha021@ucr.edu>
2020-01-19 13:23:32 +0000

    servo, email sending

---
+++ src/servo.py
@@ -0,0 +1,38 @@
+#Servo.py
+
+import RPi.GPIO as GPIO
+import time
+
+P_SERVO = 3 # adapt to your wiring
+fPWM = 50 # Hz (not higher with software PWM)
+a = 10
+b = 2
+
+def setup():
+    global pwm
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(P_SERVO, GPIO.OUT)
+    pwm = GPIO.PWM(P_SERVO, fPWM)
+    pwm.start(0)
+
+def setDirection(direction):
+    duty = a / 180 * direction + b
+    pwm.ChangeDutyCycle(duty)
+    print("direction =", direction, "-> duty =", duty)
+    time.sleep(1) # allow to settle
+
+def open():
+    print("Open starting")
+    setDirection(0)
+    print("Open done")
+
+def close():
+    print("Close starting")    
+    for direction in range(180, 0, -180):
+        setDirection(direction)
+    print("Close done")
+
+setup()
+open()
+close()
+GPIO.cleanup()
