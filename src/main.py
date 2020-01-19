375a4ba2cd8545ccc78c86f3d9974aacdd6a5e83
GrayGorilla <nbren004@ucr.edu>
2020-01-19 12:11:28 +0000

    Main file created

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import time
+from gpiozero import MotionSensor
+from camera import personOrPackage 
+
+pir = MotionSensor("BOARD7")
+
+while True:
+    # Detecting Motion
+    motion = pir.motion_detected
+    if (motion):
+        print("Motion Detected")
+        fileName, person, package = personOrPackage
+        print("Person present?: ", person)
+        print("Package present?: ", package)
+    else:
+        print("No Motion")
+    time.sleep(1)
+
